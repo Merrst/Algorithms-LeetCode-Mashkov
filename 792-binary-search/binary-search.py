44a26f553aff0539eda6cec7c1d56ad613b712ad
Merrst <maskovmaksimilian@gmail.com>
2026-02-18 18:51:10 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (27.78%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,16 +1,30 @@
 class Solution:
     def search(self, nums: list[int], target: int):
+        # начинаем поиск с самого начала списка
         left = 0
+        
+        # и до самого конца списка
         right = len(nums) - 1
 
+        # пока есть где искать
         while left <= right:
+            
+            # берем число посередине
             mid = (left + right) // 2
             
+            # если нашли нужное число
             if nums[mid] == target:
-                return mid
+                return mid  # возвращаем его индекс
+            
+            # если число меньше нужного
             elif nums[mid] < target:
+                # ищем правее
                 left = mid + 1
+            
+            # если число больше нужного
             else:
+                # ищем левее
                 right = mid - 1
                 
+        # если число так и не нашли
         return -1
